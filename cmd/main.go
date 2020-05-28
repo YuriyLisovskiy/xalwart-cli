bf36205352932d61764e5d33084d0f28134b9c49
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2020-05-28 13:47:58 +0300

    upd main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"os"
+	generators "wasp-cli/internal"
+	"wasp-cli/internal/config"
+)
 
 func main() {
-	print("Hello, Home!")
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := config.ProjectConfig{
+		WorkingDirectory:    cwd,
+		ProjectName:         "HelloWorld",
+		FrameworkName:       "wasp",
+		FrameworkNamespace:  "wasp",
+		SecretKey:           "+s6cv712&nw4gsk)1dmgpje+f#%^4lhp@!up+=p3ts+hxz(fr2",
+		CMakeCPPStandard:    17,
+		CMakeMinimumVersion: "3.13",
+	}
+
+	generators.GenerateProject(&cfg)
 }
