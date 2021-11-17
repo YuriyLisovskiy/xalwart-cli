7b632559a3dd9138f0550a266fcc04a9d233145b
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-11-17 20:51:19 +0200

    added 'version' command

--- cli/commands/root.go
+++ cli/commands/root.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"fmt"
 	"log"
+	"runtime"
+	"strings"
 
 	"github.com/YuriyLisovskiy/xalwart-cli/cli/commands/add"
 	"github.com/YuriyLisovskiy/xalwart-cli/core"
@@ -12,19 +14,26 @@ import (
 
 var rootCommand = &cobra.Command{
 	Use: core.AppName,
+	Version: core.AppVersion,
 	Short: fmt.Sprintf(
 		"%s is project and component generator tool", strcase.ToCamel(core.AppName),
 	),
 	Long: fmt.Sprintf(
-		`%s CLI tool for generating project (and it's components) written in Xalwart framework.
-Complete documentation is available at https://github.com/YuriyLisovskiy/xalwart-cli`,
-		strcase.ToCamel(core.AppName),
+		`%s CLI tool for generating project (and it's components) written in %s framework.
+Complete documentation is available at %s`,
+		strcase.ToCamel(core.AppName), strcase.ToCamel(core.FrameworkName), core.CLIAppDocumentationLink,
 	),
 }
 
 func init() {
+	rootCommand.InitDefaultVersionFlag()
+	versionTemplate := strings.TrimRight(rootCommand.VersionTemplate(), "\n")
+	versionTemplate = fmt.Sprintf("%s (%s, %s)\n", versionTemplate, runtime.GOOS, runtime.GOARCH)
+	rootCommand.SetVersionTemplate(versionTemplate)
+
 	rootCommand.AddCommand(add.RootCommand)
 	rootCommand.AddCommand(projectCommand)
+	rootCommand.AddCommand(versionCommand)
 }
 
 func Execute() {
---
+++ cli/commands/version.go
@@ -0,0 +1,23 @@
+package commands
+
+import (
+	"fmt"
+	"text/template"
+
+	"github.com/YuriyLisovskiy/xalwart-cli/core"
+	"github.com/spf13/cobra"
+)
+
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: fmt.Sprintf("Print version for %s", core.AppName),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		t := template.New("top")
+		t, err := t.Parse(rootCommand.VersionTemplate())
+		if err != nil {
+			return err
+		}
+
+		return t.Execute(cmd.OutOrStdout(), rootCommand)
+	},
+}
